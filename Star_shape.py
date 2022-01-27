c669990a896aef24c5ecf64a81cc1ac47e1b90f1
Vaibhav Gupta <47090589+Aatmsaat@users.noreply.github.com>
2022-01-27 20:39:46 +0530

    Star Shape pattern

---
+++ Star_shape.py
@@ -0,0 +1,42 @@
+#how to print following pattern in one line
+
+'''
+size: 20
+              *
+             * *
+            *   *
+           *     *
+          *       *
+*******************
+ *      *           *      *
+  *    *             *    *
+   *  *               *  *
+    **                 **
+    **                 **
+   *  *               *  *
+  *    *             *    *
+ *      *           *      *
+*******************
+          *       *
+           *     *
+            *   *
+             * *
+              *
+'''
+#Taking size of star
+n = int(input('Enter the size of star: '))+8
+
+#pattern
+pat = '*'
+
+#one fourth / quad
+h = n//4
+
+#parameters
+up = n-h-1
+
+#printing pattern
+for i in  range(n):
+	for j in range(2*up+1):
+		print('*' if (i in (h, up)) or ((i+j) in (up, up+n-1)) or ((j-i) in (up, -h)) else ' ' , end='')
+	print()
